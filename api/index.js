17ad18aff80765f4e0785b5210daa76ab0b3044e
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2025-11-25 23:43:36 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -181,7 +181,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Scoliosis Backend running on port ${PORT}`);
-});
+module.exports = app;
